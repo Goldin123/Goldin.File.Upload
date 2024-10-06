b60876240956ecea9050a6532821448ba871bbbb
Goldin Baloyi <goldin.baloyi@gitechnologies.co.za>
2024-10-06 19:32:46 +0200

    Add project files.

---
+++ Goldin.File.Upload.Api/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Goldin.File.Upload.Api
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
